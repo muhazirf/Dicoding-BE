67cd92545c4bb2428a55eaac757baa1332e008a3
20043-Fahmi Muhazir <80556314+20043-fahmi@users.noreply.github.com>
2023-02-04 20:03:09 +0700

    fileSystem

---
+++ nodejsBasic/filseSystem/index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+const path = require('path');
+
+path.resolve(__dirname, 'todo.txt');
+
+const fileReadCallback = (error, data) => {
+    if(error) {
+        console.log('Gagal membaca berkas');
+        return;
+    }
+    console.log(data);
+};
+ 
+fs.readFile('todo.txt', 'UTF-8', fileReadCallback);
