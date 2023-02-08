9421f1a3d6da893cc7b8021bb8c403b2021459b9
20043-Fahmi Muhazir <80556314+20043-fahmi@users.noreply.github.com>
2023-02-08 14:02:34 +0700

    membuat web-server http

--- hapi-web-server/server.js
+++ hapi-web-server/server.js
@@ -1,1 +1,15 @@
 console.log("Halo, kita akan belajar membuat server menggunakan Hapi");
+
+const hapi = require("@hapi/hapi");
+
+const init = async () => {
+    const server = hapi.server({
+        port: 5000,
+        host: 'localhost',
+    });
+
+    await server.start(
+        console.log(`Server berjalan pada ${server.info.uri}`));      
+};
+
+init();
