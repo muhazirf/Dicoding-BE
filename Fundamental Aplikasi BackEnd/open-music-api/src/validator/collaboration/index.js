59db41101a9c1a073930da2523561ec0c2da54d0
Fahmi Muhazir <fahmimuhazir12@gmail.com>
2023-08-22 02:59:56 +0700

    feature: all validator

---
+++ Fundamental Aplikasi BackEnd/open-music-api/src/validator/collaboration/index.js
@@ -0,0 +1,16 @@
+const { CollaborationPayloadSchema } = require('./schema')
+const InvariantError = require('../../exceptions/InvariantError')
+
+class CollaborationValidator {
+  validateCollaborationPayload (payload) {
+    const validationResult = CollaborationPayloadSchema.validate(payload)
+
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message)
+    }
+
+    return validationResult.value
+  }
+}
+
+module.exports = CollaborationValidator
